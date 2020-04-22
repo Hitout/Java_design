c570895a1d8d5a3c187b04f404ad06ae05677e94
gxyan <1412564124@qq.com>
2020-04-22 18:33:35 +0800

    update

--- src/main/java/com/spring/ioc/annotation/AppConfig.java
+++ src/main/java/com/spring/ioc/annotation/AppConfig.java
@@ -14,8 +14,6 @@ public class AppConfig {
     /**
      * 注解@Bean在返回实例的方法上使用，如果未通过@Bean指定bean的名称，则默认与标注的方法名相同
      * 默认作用域为singleton单例作用域，通过@Scope设置作用域
-     *
-     * @return
      */
     @Bean
     public IMessageService messageService() {
