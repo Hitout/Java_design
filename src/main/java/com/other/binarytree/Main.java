fd9da0c95e0a69bdfc68a2f4df44c0068e754efc
gxyan <gxyancn@qq.com>
2021-03-26 14:48:03 +0800

    二叉树

--- src/main/java/com/other/binarytree/Main.java
+++ src/main/java/com/other/binarytree/Main.java
@@ -183,8 +183,8 @@ public class Main {
         //  2     3
         // /     / \
         // 4    5   7
-        //  \  /
-        //   6 8
+        //       \  /
+        //        6 8
         BinaryTreeNode root = new BinaryTreeNode(1);
         BinaryTreeNode node2 = new BinaryTreeNode(2);
         BinaryTreeNode node3 = new BinaryTreeNode(3);
